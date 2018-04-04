fd8f8c7b6b80761dc53634fc5f1d2aa76735b89a
Leila Ling <leila@getpopspots.com>
2018-04-04 16:27:04 -0500

    Added current debt to calculator

--- Site/js/calculator.js
+++ Site/js/calculator.js
@@ -9,23 +9,27 @@ function addCard() {
 	if(numCards !== 3) {
 		$('#credit-balance').append(
 			`<div class="form-row card-balance">
-	      <div class="form-group col-md-6">
-	        <label for="bankName">Bank Name</label>
-	        <input type="text" class="form-control bankName" placeholder="Bank Name">
-	      </div>
-	      <div class="form-group col-md-2">
-	        <label for="APR">APR</label>
-	        <input type="text" class="form-control APR" placeholder="APR">
-	      </div>
-	      <div class="form-group col-md-3">
-	        <label for="annualFee">Annual Fee</label>
-	        <input type="text" class="form-control annualFee" placeholder="Annual Fee">
-	      </div>
-	      <div class="form-group col-md-1">
-	        <label></label>
-	        <a class="clear">X</a>
-	      </div>
-	    </div>`);
+        <div class="form-group col-lg-4">
+          <label for="bankName">Bank Name</label>
+          <input type="text" class="form-control bankName" placeholder="Bank Name">
+        </div>
+        <div class="form-group col-lg-2">
+          <label for="APR">APR</label>
+          <input type="text" class="form-control APR" placeholder="APR">
+        </div>
+        <div class="form-group col-lg-2">
+          <label for="annualFee">Annual Fee</label>
+          <input type="text" class="form-control annualFee" placeholder="Annual Fee">
+        </div>
+        <div class="form-group col-lg-3">
+          <label for="annualFee">Current Debt</label>
+          <input type="text" class="form-control annualFee" placeholder="Current Debt">
+        </div>
+        <div class="form-group col-lg-1">
+          <label></label>
+          <a class="clear">X</a>
+        </div>
+      </div>`);
 
 		var numCards = $('#credit-balance .card-balance').length;
 		
